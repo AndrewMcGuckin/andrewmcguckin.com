a49ac2b3470dbf4f29f7fca6b00d9fb4d29a173f
AndrewMcGuckin <github@andrewmcguckin.com>
2023-01-26 13:06:08 +0000

    refactor(home): update description and hero text
    remove unused tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,10 +38,5 @@ module.exports = {
     },
   },
   variants: {},
-  plugin: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
+  plugin: [],
 }
